1938b7a7ed8a5f2213c8aaed4adcd6737f7357f8
Jan-Philipp Litza <janphilipp@litza.de>
2021-02-07 20:49:02 +0100

    Set trust_env=False on httpx client
    This makes it not read .netrc, which does not work in certain cases (and
    also is not necessary)

--- nina_xmpp/__init__.py
+++ nina_xmpp/__init__.py
@@ -88,7 +88,7 @@ class NinaXMPP:
             await asyncio.sleep(self.config['check_interval'])
 
     async def update_feeds(self):
-        async with httpx.AsyncClient() as http_client:
+        async with httpx.AsyncClient(trust_env=False) as http_client:
             for url in self.config['feeds']:
                 try:
                     feed = self.db.query(Feed).filter_by(url=url).one()
