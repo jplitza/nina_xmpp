c4f6f2c2c964a997f23d9a4d9f2c2d8f837b23b3
Jan-Philipp Litza <janphilipp@litza.de>
2022-06-05 15:39:19 +0200

    Add support for logging to syslog
    While systemd captures stderr, it has no notion of priority there.

--- nina_xmpp/__main__.py
+++ nina_xmpp/__main__.py
@@ -1,16 +1,38 @@
 import asyncio
+import logging
+import logging.handlers
+
+from argparse_logging import add_log_level_argument, LoggingAction
 
 from . import NinaXMPP
+
+
+class LogHandler(LoggingAction):
+    def configure(self, path):
+        if path:
+            logging.getLogger().addHandler(
+                logging.handlers.SysLogHandler(address=path)
+            )
+
+
+def add_log_handler_argument(parser, option_string="--syslog"):
+    return parser.add_argument(
+        option_string,
+        help="Enable logging to syslog at given address",
+        metavar="ADDRESS",
+        type=str,
+        action=LogHandler,
+    )
 
 
 def main():
     import yaml
     import argparse
-    from argparse_logging import add_log_level_argument
 
     parser = argparse.ArgumentParser()
     parser.add_argument('config_file', type=argparse.FileType('r'))
     add_log_level_argument(parser)
+    add_log_handler_argument(parser)
     args = parser.parse_args()
 
     config = yaml.safe_load(args.config_file)
